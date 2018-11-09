b4b3c09192360776ce9eebbcebd27046ae51a3fb
Matthew Markowitz <mmarkow2@stevens.edu>
2018-11-08 19:20:07 -0500

    parameter tuning

--- Code.py
+++ Code.py
@@ -18,30 +18,35 @@ def LARSGradient(negDXt, Hessian, xt, alpha, lambdaParm):
 trainingSet = "trainingData/"
 testingSet = "testingData/"
 alphaIterations = 1000
-dictIterations = 20
+dictIterations = 50
 
 #sampling rate for our dataset is 22050
-sampling_rate_target = 500
-atoms = sampling_rate_target * 2
-A = numpy.zeros((atoms, atoms),  numpy.float64)
-B = numpy.zeros((sampling_rate_target, atoms),  numpy.float64)
-D = numpy.random.randn(sampling_rate_target, atoms)
+sampling_rate_target = 5000
+dictionary_size = 200
+atoms = dictionary_size * 5
+A = numpy.zeros((atoms, atoms),  numpy.float32)
+B = numpy.zeros((dictionary_size, atoms),  numpy.float32)
+D = numpy.random.randn(dictionary_size, atoms)
+D = D.astype(numpy.float32)
 
 lambdaParm = 1
 for dataLocation in [trainingSet, testingSet]:
+    count = 0
     for filename in os.listdir(dataLocation):
         if filename.endswith(".wav"): 
+            print("Sound: " + str(count))
+            count += 1
             audio, sampling_rate = librosa.load(dataLocation + filename, sampling_rate_target)
             if (dataLocation == testingSet):
-                audioOutput = numpy.array([])
-            for i in range(math.ceil(audio.shape[0] / sampling_rate)):
+                audioOutput = numpy.array([], numpy.float32)
+            for i in range(math.ceil(audio.shape[0] / dictionary_size)):
                 #extract an xt for each second of audio, padding shorter audio to reach a second
-                xt = numpy.copy(audio[(i*sampling_rate):((i+1)*sampling_rate)])
-                xt = numpy.pad(xt, pad_width=(0,sampling_rate - xt.shape[0]), mode='constant', constant_values=0)
+                xt = numpy.copy(audio[(i*dictionary_size):((i+1)*dictionary_size)])
+                xt = numpy.pad(xt, pad_width=(0, dictionary_size - xt.shape[0]), mode='constant', constant_values=0)
                 xt = xt.reshape((xt.shape[0], 1))
 
                 #LARS alpha computation
-                alpha = numpy.zeros((atoms, 1), numpy.float64)
+                alpha = numpy.zeros((atoms, 1), numpy.float32)
 
                 #precompute values from the unchanging dictionary for efficiency
                 hessian = numpy.matmul(numpy.transpose(D), D)
@@ -76,7 +81,7 @@ for dataLocation in [trainingSet, testingSet]:
                 print("Training audio file processed")
             else:
                 #output the representation
-                librosa.output.write_wav('Output' + filename, audioOutput.astype(numpy.float32), sampling_rate)
+                librosa.output.write_wav('Output' + filename, audioOutput, sampling_rate)
                 print("Testing audio file processed")
             
 #plt.figure(figsize=(12, 4))
