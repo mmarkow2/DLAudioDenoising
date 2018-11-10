0d9ad7d43fb5941d7432e82a8b885eb3808bc1b9
Matthew Markowitz <mmarkow2@stevens.edu>
2018-11-10 11:58:05 -0500

    Switched to SKlearn LARS implementation

--- Code.py
+++ Code.py
@@ -3,33 +3,29 @@ import math
 import numpy
 import librosa
 import librosa.display
+from sklearn.linear_model import LassoLars
 import matplotlib.pyplot as plt
-
-def normGrad(alpha):
-    result = numpy.copy(alpha)
-    result[result<0] = -1
-    result[result>=0] = 1
-    return result
-
-def LARSGradient(negDXt, Hessian, xt, alpha, lambdaParm):
-    return  negDXt + numpy.matmul(Hessian, alpha) + lambdaParm * normGrad(alpha)
 
 
 dataSet = "Data/"
 resultSet = "Result/"
-alphaIterations = 100000
-dictIterations = 1000
+dictIterations = 100
 
 #sampling rate for our dataset is 22050
 sampling_rate_target = 5000
-dictionary_size = 200
-atoms = dictionary_size * 5
-A = numpy.zeros((atoms, atoms),  numpy.float32)
+dictionary_size = 50
+atoms = dictionary_size * 10
+A = numpy.zeros((atoms, atoms),  numpy.float32) + 0.000001
 B = numpy.zeros((dictionary_size, atoms),  numpy.float32)
 D = numpy.random.randn(dictionary_size, atoms)
 D = D.astype(numpy.float32)
 
-lambdaParm = 1
+#enforce condition that all columns have less than unit norm
+D = D / numpy.linalg.norm(D, 2, 0, True)
+
+#LARS alpha initialization
+reg = LassoLars(alpha=0.0005, fit_intercept=False, copy_X=True)
+
 count = 0
 for filename in os.listdir(dataSet):
     if filename.endswith(".wav"): 
@@ -41,28 +37,15 @@ for filename in os.listdir(dataSet):
             #extract an xt for each chunk of audio, padding shorter audio to reach a second
             xt = numpy.copy(audio[(i*dictionary_size):((i+1)*dictionary_size)])
             xt = numpy.pad(xt, pad_width=(0, dictionary_size - xt.shape[0]), mode='constant', constant_values=0)
-            xt = xt.reshape((xt.shape[0], 1))
 
             #LARS alpha computation
-            alpha = numpy.zeros((atoms, 1), numpy.float32)
-
-            #precompute values from the unchanging dictionary for efficiency
-            hessian = numpy.matmul(numpy.transpose(D), D)
-            strConvexAlpha = min(numpy.linalg.eigvalsh(hessian))
-            #-D * xt is used in the gradient but is precomputed since it doesn't depend on alpha
-            negDXt = numpy.matmul(-numpy.transpose(D), xt)
-
-            for t in range(1, alphaIterations + 1):
-                print("Finished " + str(t) + "/" + str(alphaIterations) + " Alpha Iterations", end="\r")
-                gradient = LARSGradient(negDXt, hessian, xt, alpha, lambdaParm)
-                #theoretical learning_rate
-                if (strConvexAlpha > 0 ):
-                    #when strongly-convex, use 2/(alpha*t)
-                    learning_rate = 2 / (strConvexAlpha * t)
-                else:
-                    #when not strongly-convex, use 1/(sqrt(t) * norm(subradient))
-                    learning_rate = 1 / (math.sqrt(t) * numpy.linalg.norm(gradient))
-                alpha = alpha - learning_rate * gradient
+            reg.fit(D, xt)
+            alpha = reg.coef_
+            alpha = alpha.astype(numpy.float32)
+            
+            #reshape parameters
+            xt = xt.reshape((xt.shape[0], 1))
+            alpha = alpha.reshape((alpha.shape[0], 1))
 
             print("Finished Alpha Update")
             A += numpy.matmul(alpha, numpy.transpose(alpha))
